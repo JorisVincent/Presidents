d6db0e9a2f45bef0fe2a9a1d84966655dd7c7a92
Marko <markopertak@gmail.com>
2023-01-27 15:47:39 +0100

    Final version of code pretty sure

--- Alma_maters_president_grouped.R
+++ Alma_maters_president_grouped.R
@@ -1,5 +1,9 @@
 library("tidyverse")
 library("ggplot2")
+library("emojifont")
+
+load.fontawesome()
+leaf <- fontawesome('fa-leaf')
 
 # Load data
 uni_data <- read_csv("president_alma_maters.csv") 
@@ -22,11 +26,17 @@ uni_data <- uni_data |>
   mutate(party_label = if_else(startsWith(party_label,"Whig"),"Other",party_label)) |>
   mutate(party_label = if_else(startsWith(party_label,"Know"),"Other",party_label)) |>
   mutate(party_label = if_else(startsWith(party_label,"None"),"Other",party_label)) |>
-  group_by(party_label)
+  mutate(is_ivy = education %in% c("Harvard University", "Yale University", "Princeton Univeristy", "University of Pennsylvania", "Columbia University"))
 
 # Bar graph
 barred_data <- uni_data  |>
-  count(education)
+  group_by(party_label, education) |>
+  summarise(n=n())
+
+label_data <- uni_data |>
+  filter(is_ivy) |>
+  group_by(education) |>
+  summarise(total_n=n())
 
 mycolors <- c("Democrat"="#0000FF", "Other"="#808080", "Republican"="#FF0000")
 
@@ -34,9 +44,10 @@ barred_data |>
   group_by(education) |>
   mutate(total_n = sum(n)) |>
   ungroup() |>
-  ggplot(aes(x = reorder(education, total_n), y = n, fill = party_label)) +
+  ggplot(aes(x = reorder(education, total_n), y = n)) +
   coord_flip() +
-  geom_col() +
+  geom_col(aes(fill = party_label)) +
+  geom_text(aes(y=total_n+0.25), label=leaf, family='fontawesome-webfont', data=label_data) +
   theme_light()+
   scale_fill_manual(values=mycolors)  +
   scale_y_continuous(breaks = c(0,1,2,3,4,5,6,7,8), minor_breaks=NULL) +
--- ivy_league_presidents.R
+++ ivy_league_presidents.R
@@ -12,18 +12,18 @@ uni_data <- uni_data |>
   mutate(education = if_else(startsWith(education,"University of North Carolina"),"University of North Carolina",education)) |>
   mutate(education = if_else(startsWith(education,"University of Cincinnati"),"University of Cincinnati",education)) |>
   mutate(education = if_else(startsWith(education,"University of Virginia"),"University of Virginia",education)) |>
-  mutate(education = if_else(startsWith(education,"Columbia University"),"Columbia",education)) |>
+  mutate(education = if_else(startsWith(education,"Columbia"),"Columbia University",education)) |>
   mutate(education = if_else(startsWith(education,"Albany Law"),"University at Albany",education)) |>
   mutate(education = if_else(startsWith(education,"X"),"Without Tertiary Education",education)) |>
   mutate(education = if_else(startsWith(education,"Northampton"),"University of Northampton",education)) |>
-  mutate(education = ifelse(education %in% c("Harvard University", "Yale University", "Princeton Univeristy", "University of Pennsylvania"),"Ivy League", education)) |>
+  mutate(education = ifelse(education %in% c("Harvard University", "Yale University", "Princeton Univeristy", "University of Pennsylvania", "Columbia University"),"Ivy League", education)) |>
   mutate(education = ifelse(education %in% c("Ivy League", "Without Tertiary Education"), education, "Not Ivy League"))
 
 uni_data |>
   ggplot(aes(y = education, x = birth_date)) +
-  geom_point(shape = 125, size=15, alpha=0.5, colour='purple') + # shape=124 sets the points to be the pipe-symbol "|"
+  geom_point(shape = 124, size=15, alpha=0.5, colour='purple') + # shape=124 sets the points to be the pipe-symbol "|"
   theme_light() +
   ylab("") +
   xlab("Year")
-ggsave("Violinplot_Presidents.pdf", width = 7, height = 4)
+ggsave("Barcode_Presidents.pdf", width = 7, height = 4)
 
